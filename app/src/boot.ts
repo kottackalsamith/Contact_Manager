a89939a7033f4afed46a322739b3862f369bf342
samith <kottackalsamith@gmail.com>
2016-10-24 20:37:45 +0530

    controller added

---
+++ app/src/boot.ts
@@ -0,0 +1,7 @@
+/// <reference path="_all.ts" />
+
+var ContactManagerApp;
+
+(function(ContactManagerApp) {
+    angular.module('contactManagerApp', ['ngMaterial']);
+})(ContactManagerApp || (ContactManagerApp = {}));
