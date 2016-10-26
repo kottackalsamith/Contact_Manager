d69dd2a902141381bfe61b5b4e34edb664fde148
samith <kottackalsamith@gmail.com>
2016-10-26 16:45:10 +0530

    Remove notes  added

--- app/src/controllers/mainController.ts
+++ app/src/controllers/mainController.ts
@@ -2,9 +2,10 @@
 
 module ContactManagerApp {
     export class MainController {
-        static $inject = ['userService', '$mdSidenav'];
+        static $inject = ['userService', '$mdSidenav', '$mdToast'];
         constructor(private userService: IUserService,
-            private $mdSidenav: angular.material.ISidenavService) {
+            private $mdSidenav: angular.material.ISidenavService,
+            private $mdToast: angular.material.IToastService) {
             var self = this;
 
             this.userService
@@ -35,6 +36,21 @@ module ContactManagerApp {
             }
 
             this.tabIndex = 0;
+        }
+
+        removeNote(note: Note): void {
+            var foundIndex = this.selected.notes.indexOf(note);
+            this.selected.notes.splice(foundIndex, 1);
+            this.openToast("Note was removed");
+        }
+
+        openToast(message: string): void {
+            this.$mdToast.show(
+                this.$mdToast.simple()
+                    .textContent(message)
+                    .position('top right')
+                    .hideDelay(3000)
+            );
         }
     }
 }
