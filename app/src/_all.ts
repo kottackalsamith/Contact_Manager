60ef177abb01a837b2f1f6774c18fe9781c2ca77
samith <kottackalsamith@gmail.com>
2016-10-26 19:14:04 +0530

    User add button function implemented

--- app/src/_all.ts
+++ app/src/_all.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../typings/index.d.ts" />
 /// <reference path="boot.ts" />
 /// <reference path="./controllers/mainController.ts" />
+/// <reference path="./controllers/addUserDialogController.ts" />
 /// <reference path="./services/userService.ts" />
 /// <reference path="models.ts" />
